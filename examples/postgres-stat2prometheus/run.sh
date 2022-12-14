e0e72b8e70cbfd8b985fa1b26a0ac0a2555cc137
yanagitani <takanori.yanagitani@gmail.com>
2022-12-14 10:45:50 +0900

    docs: more sample metrics settings

--- examples/postgres-stat2prometheus/run.sh
+++ examples/postgres-stat2prometheus/run.sh
@@ -9,9 +9,10 @@ export PGDATABASE=postgres
 export OTEL_SERVICE_NAME=test-pg2otel
 
 names(){
-    echo sample.d/pg12.pg_stat_database.toml
+    #echo sample.d/pg12.pg_stat_database.toml
     #echo sample.d/pg_stat_database.toml
-    echo sample.d/pg_stat_activity.toml
+    echo sample.d/pg_stat_wal.toml
+    #echo sample.d/pg_stat_activity.toml
 }
 
 main(){
