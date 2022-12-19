e994f48f978ca13a19bc22d4291357b45fbf1886
yanagitani <takanori.yanagitani@gmail.com>
2022-12-19 09:08:17 +0900

    docs: more docs

--- src/label.rs
+++ src/label.rs
@@ -1,9 +1,12 @@
+//! Metrics label
+
 use std::collections::BTreeMap;
 
 use crate::evt::Event;
 
 use opentelemetry::{KeyValue, Value};
 
+/// Metrics label config info.
 #[derive(serde::Deserialize)]
 pub struct Label {
     name: String,
@@ -11,14 +14,17 @@ pub struct Label {
 }
 
 impl Label {
+    /// Gets the name.
     pub fn as_name(&self) -> &str {
         self.name.as_str()
     }
 
+    /// Gets the description.
     pub fn as_desc(&self) -> &str {
         self.description.as_str()
     }
 
+    /// Converts to `KeyValue` if the map contains the label value.
     pub fn to_kv(&self, m: &BTreeMap<String, Value>) -> Result<KeyValue, Event> {
         let v: &Value = m.get(self.name.as_str()).ok_or_else(|| {
             Event::LabelValueNotFound(format!("Value for label missing: {}", self.name))
@@ -26,6 +32,9 @@ impl Label {
         Ok(KeyValue::new(self.name.clone(), v.clone()))
     }
 
+    /// Converts labels to `KeyValue`s.
+    ///
+    /// Labels without a value will be ignored.
     pub fn to_attrs(s: &[Self], m: &BTreeMap<String, Value>) -> Vec<KeyValue> {
         s.iter().flat_map(|l: &Self| l.to_kv(m).ok()).collect()
     }
