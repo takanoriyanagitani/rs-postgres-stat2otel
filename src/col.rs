927fa95e618181439109484910e590c459a550f4
yanagitani <takanori.yanagitani@gmail.com>
2022-12-23 12:33:44 +0900

    docs: more docs

--- src/col.rs
+++ src/col.rs
@@ -1,19 +1,23 @@
 use opentelemetry::Value;
 
+/// A metrics value(with name) container.
 pub struct Column {
     name: String,
     value: Value,
 }
 
 impl Column {
+    /// Gets a name.
     pub fn as_name(&self) -> &str {
         self.name.as_str()
     }
 
+    /// Gets a value.
     pub fn as_value(&self) -> &Value {
         &self.value
     }
 
+    /// Creates a new name/integer container.
     pub fn new_integer(name: String, value: i64) -> Self {
         Self {
             name,
@@ -21,6 +25,7 @@ impl Column {
         }
     }
 
+    /// Creates a new name/float container.
     pub fn new_float(name: String, value: f64) -> Self {
         Self {
             name,
@@ -28,6 +33,7 @@ impl Column {
         }
     }
 
+    /// Creates a new name/string container.
     pub fn new_string(name: String, value: String) -> Self {
         Self {
             name,
